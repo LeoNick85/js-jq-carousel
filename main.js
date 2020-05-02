84846570fc756453ee07a9bac8c63560fdf1b4e9
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-05-02 18:02:15 +0200

    Version 1.0

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ $(".next").click(function() {
     var current_image = $("img.active");
     current_image.removeClass("active");
 
-    //Individuo l'elemento successivo. Se presente, sposto l'active all'elemento successivo, altrimenti riparto dal primo elemento
+    //Individuo il pallino attivo e tolgo l'active-dot
+    var current_dot = $(".active-dot");
+    current_dot.removeClass("active-dot");
+
+    //Individuo l'elemento successivo e il relativo pallino. Se presente, sposto l'active all'elemento successivo, altrimenti riparto dal primo elemento
     var next_image = current_image.next();
+    var next_dot = current_dot.next();
 
     if (next_image.length != 0) {
         next_image.addClass("active");
+        next_dot.addClass("active-dot");
     } else {
         $(".photos img:first-child").addClass("active");
+        $(".img-selector .fa-circle:first-child").addClass("active-dot");
     }
 })
 
@@ -22,12 +29,19 @@ $(".prev").click(function() {
     var current_image = $("img.active");
     current_image.removeClass("active");
 
-    //Individuo l'elemento successivo. Se presente, sposto l'active all'elemento successivo, altrimenti riparto dal primo elemento
+    //Individuo il pallino attivo e tolgo l'active-dot
+    var current_dot = $(".active-dot");
+    current_dot.removeClass("active-dot");
+
+    //Individuo l'elemento e il pallino successivo. Se presente, sposto l'active all'elemento successivo, altrimenti riparto dal primo elemento
     var prev_image = current_image.prev();
+    var prev_dot = current_dot.prev();
 
     if (prev_image.length != 0) {
         prev_image.addClass("active");
+        prev_dot.addClass("active-dot");
     } else {
         $(".photos img:last-of-type").addClass("active");
+        $(".img-selector .fa-circle:last-child").addClass("active-dot");
     }
 })
